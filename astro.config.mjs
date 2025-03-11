9773e66e7f4f0f7b8793ae254141825de6aa150d
Pritam Roy <roypritam18@gmail.com>
2025-03-11 13:01:24 +0530

    new last day

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  
   }
 });
+
+
+
